674e7244a50b5ed402f1cc2acec21be5cca37911
Ronald Libago <code@ronaldlibago.com>
2019-06-16 11:09:10 -0700

    Add Me component

---
+++ src/components/me/me.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MeSection = styled.section``;
+
+const Me = () => {
+    return (<MeSection>Hey</MeSection>);
+}
+
+export default Me;
